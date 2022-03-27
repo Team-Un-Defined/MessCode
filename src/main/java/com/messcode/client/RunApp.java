eb5e3ff4ec4b1211571acdaae82b74ae1a6405b0
Noemi Farkas <xfarkasn@stuba.sk>
2022-03-27 18:38:55 +0200

    Logging_done

--- src/main/java/com/messcode/client/RunApp.java
+++ src/main/java/com/messcode/client/RunApp.java
@@ -6,18 +6,14 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import com.messcode.client.core.ModelFactory;
 import com.messcode.client.core.ViewHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
+
 
 public class RunApp extends Application {
     @Override
     public void start(Stage stage) {
 
-        //Logger logger = LoggerFactory.getLogger(RunApp.class);
-
+       
         ClientFactory cf = new ClientFactory();
         ModelFactory mf = new ModelFactory(cf);
         ViewModelFactory vmf = new ViewModelFactory(mf);
--- src/main/java/com/messcode/client/Start.java
+++ src/main/java/com/messcode/client/Start.java
@@ -1,9 +1,16 @@
 package com.messcode.client;
-
 import javafx.application.Application;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class Start {
+  private static final Logger log4j = LogManager.getLogger(RunApp.class);
   public static void main(String[] args) {
+        log4j.debug("Debug Message Logged !!!", new NullPointerException("NullError"));
+      log4j.error("Error Message Logged !!!", new NullPointerException("NullError"));
+      log4j.info("INFO INFO !!!", new NullPointerException("NullError"));
+      
     Application.launch(RunApp.class);
   }
 }
