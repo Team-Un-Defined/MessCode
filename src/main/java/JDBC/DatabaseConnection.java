e3cff152bf575447c609fdaa03a97cf47a283b41
Noemi Farkas <xfarkasn@stuba.sk>
2022-04-12 11:35:27 +0200

    Merged the registration branch

--- src/main/java/JDBC/DatabaseConnection.java
+++ src/main/java/JDBC/DatabaseConnection.java
@@ -12,7 +12,7 @@ package JDBC;
 public class DatabaseConnection {
 private final String conn = "jdbc:postgresql://localhost:5432/MessCode";
 private final String name =  "postgres";
-private final String pass =  "ih de TRfvj85Jl 55 igh 94";
+private final String pass =  "XXXX";
 
     public String getConn() {
         return conn;
