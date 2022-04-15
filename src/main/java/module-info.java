bd16ac1c766bc2cdad413f667a85e10362e2debb
RomanKorzhov <71638090+RomanKorzhov@users.noreply.github.com>
2022-04-15 12:27:15 +0200

    Git Fix

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module com.example.messcode {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+    requires org.controlsfx.controls;
+    requires java.sql;
+
+    opens com.messcode.client.views.login to javafx.fxml;
+    opens com.messcode.client.views.chat to javafx.fxml;
+    opens com.messcode.client.views.new_employee to javafx.fxml;
+    opens com.messcode.client.views.new_group to javafx.fxml;
+    opens com.messcode.client.views.change_password to javafx.fxml;
+    exports com.messcode.client.views.chat;
+    exports com.messcode.client.core;
+    exports com.messcode.client;
+    requires org.apache.logging.log4j;
+    requires org.apache.logging.log4j.core;
+    requires org.postgresql.jdbc;
+}
