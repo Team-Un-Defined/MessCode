cb3b6cd133dc26ec51a179f27ea74dbd4a7308c3
Nao <nojika99gmail.com>
2022-04-21 18:20:45 +0200

    logger caused bug fixed

--- src/main/java/com/messcode/client/Start.java
+++ src/main/java/com/messcode/client/Start.java
@@ -1,15 +1,16 @@
 package com.messcode.client;
 
+import JDBC.ImportData;
 import javafx.application.Application;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
 
 public class Start {
 
     public static void main(String[] args) {
+       java.util.logging.Logger.getLogger(Start.class.getName()).log(Level.SEVERE, "sadsd");
         Application.launch(RunApp.class);
     }
 }
