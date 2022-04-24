392340b2b63de92ab5718b638fb3329d81fb34cc
Kamilla Kisova <kis.kamilla2000@gmail.com>
2022-04-24 22:35:43 +0200

    started view profile panel

--- src/main/java/com/messcode/client/views/view_profile/ViewProfileController.java
+++ src/main/java/com/messcode/client/views/view_profile/ViewProfileController.java
@@ -1,2 +1,26 @@
-package com.messcode.client.views.view_profile;public class ViewProfileController {
+package com.messcode.client.views.view_profile;
+
+import com.messcode.client.core.ViewHandler;
+import com.messcode.transferobjects.User;
+import javafx.scene.control.Label;
+
+import java.util.ResourceBundle;
+
+public class ViewProfileController {
+    public Label nameLabel;
+    public Label firstNameLabel;
+    public Label lastNameLabel;
+    public Label emailLabel;
+    public Label roleLabel;
+
+    private ViewProfileViewModel viewProfileVM;
+    private ViewHandler vh;
+    private User user;
+    private ResourceBundle bundle;
+
+    public void init(ViewProfileViewModel removeUserVM, ViewHandler vh, ResourceBundle bundle) {
+        this.viewProfileVM = removeUserVM;
+        this.vh = vh;
+        this.bundle = bundle;
+    }
 }
