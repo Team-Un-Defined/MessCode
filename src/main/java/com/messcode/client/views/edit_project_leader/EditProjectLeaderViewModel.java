28344e097ccd00abe7ebaaf4ecf01b29b0d4fb12
Kamilla Kisova <kis.kamilla2000@gmail.com>
2022-04-24 17:15:22 +0200

    messages' texts are now copied upon clicks

--- src/main/java/com/messcode/client/views/edit_project_leader/EditProjectLeaderViewModel.java
+++ src/main/java/com/messcode/client/views/edit_project_leader/EditProjectLeaderViewModel.java
@@ -4,15 +4,14 @@ import com.messcode.client.model.MainModel;
 import com.messcode.transferobjects.Group;
 import com.messcode.transferobjects.User;
 import com.messcode.transferobjects.util.Subject;
-import java.beans.PropertyChangeEvent;
-
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
-import java.sql.Statement;
-import java.util.ArrayList;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
 
 public class EditProjectLeaderViewModel implements Subject {
     private Group selectedGroup;
@@ -22,15 +21,13 @@ public class EditProjectLeaderViewModel implements Subject {
 
     public EditProjectLeaderViewModel(MainModel mainModel) {
         support = new PropertyChangeSupport(this);
-        usersList= FXCollections.observableArrayList();
+        usersList = FXCollections.observableArrayList();
         this.mainModel = mainModel;
-        
-        mainModel.addListener("changeSelectedGroup",this::updateGroup);
+
+        mainModel.addListener("changeSelectedGroup", this::updateGroup);
         mainModel.addListener("AddOfflineUsers", this::addOfflineUsers);
-        
     }
-    
-    
+
     private void addOfflineUsers(PropertyChangeEvent propertyChangeEvent) {
         ArrayList<User> users = (ArrayList<User>) propertyChangeEvent.getNewValue();
 
@@ -44,12 +41,12 @@ public class EditProjectLeaderViewModel implements Subject {
             System.out.println(usersList);
         });
     }
-     private void updateGroup(PropertyChangeEvent propertyChangeEvent) {
-        selectedGroup = (Group)propertyChangeEvent.getNewValue();
-  
+
+    private void updateGroup(PropertyChangeEvent propertyChangeEvent) {
+        selectedGroup = (Group) propertyChangeEvent.getNewValue();
+
     }
-    
-    
+
     @Override
     public void addListener(String eventName, PropertyChangeListener listener) {
         support.addPropertyChangeListener(eventName, listener);
@@ -63,24 +60,20 @@ public class EditProjectLeaderViewModel implements Subject {
     public ObservableList<User> getUsers() {
         ObservableList<User> use = FXCollections.observableArrayList();
         use.addAll(usersList);
-        use.removeIf( i ->i.getEmail().equals(selectedGroup.getLeader().getEmail()));
+        use.removeIf(i -> i.getEmail().equals(selectedGroup.getLeader().getEmail()));
         return use;
     }
-    
-   public User getLeader(){
-   
-  return selectedGroup.getLeader();
-          
-   }
+
+    public User getLeader() {
+        return selectedGroup.getLeader();
+    }
 
     void changeLeader(User u) {
-        if(u.getEmail().equals(selectedGroup.getLeader().getEmail())){
-        return;
+        if (u.getEmail().equals(selectedGroup.getLeader().getEmail())) {
+            return;
         }
-        Group g = new Group(selectedGroup.getName(),selectedGroup.getDescription(),u);
+        Group g = new Group(selectedGroup.getName(), selectedGroup.getDescription(), u);
         mainModel.changeLeader(g);
-        
+
     }
-    
-    
 }
